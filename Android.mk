ec06ad37abfab5c2d6b294c8946eab3e5e9167d4
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-23 00:35:16 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,6 +6,5 @@ LOCAL_SRC_FILES := avc_bypass.c
 
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
 LOCAL_LDFLAGS := -fPIE -pie
-LOCAL_LDLIBS := -lpthread
 
 include $(BUILD_EXECUTABLE)
