ba0502a5b86434ce30ba7a1dcdce0130d33660cb
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-23 00:39:52 +0900

    Add pthread flag to LOCAL_CFLAGS in Android.mk

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := root_shell
 LOCAL_SRC_FILES := root_shell.c
 
-LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
+LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format -pthread
 LOCAL_LDFLAGS := -fPIE -pie
 
 include $(BUILD_EXECUTABLE)
