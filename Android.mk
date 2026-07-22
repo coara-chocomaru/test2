ecfce4291a8f5b35f31dc0a95134b3c65a09e9cf
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-23 00:06:41 +0900

    Rename module from avc_bypass to root_shell

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := avc_bypass
-LOCAL_SRC_FILES := avc_bypass.c
+LOCAL_MODULE := root_shell
+LOCAL_SRC_FILES := root_shell.c
 
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
 LOCAL_LDFLAGS := -fPIE -pie
