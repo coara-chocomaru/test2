0b6a39c074a19e33773953f1e1617557fa502a1f
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-24 23:05:48 +0900

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := avc_bypass
-LOCAL_SRC_FILES := avc_bypass.c
-
+LOCAL_MODULE := root_shell
+LOCAL_SRC_FILES := main/main.c util/kgsl_ops.c util/cache_ops.c util/kaslr.c util/common.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/h
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
 LOCAL_LDFLAGS := -fPIE -pie
 
