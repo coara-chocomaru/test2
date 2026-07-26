d1049f54de19000b9f3f91f66b8057cfe8c0f2b1
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-27 01:22:55 +0900

    Rename module from avc_bypass to check_poc

--- Android.mk
+++ Android.mk
@@ -1,8 +1,10 @@
+# Android.mk
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := avc_bypass
-LOCAL_SRC_FILES := avc_bypass.c
+LOCAL_MODULE := check_poc
+LOCAL_SRC_FILES := check_poc.c
+
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
 LOCAL_LDFLAGS := -fPIE -pie
 
