30f0119b449cac3497140458fc96b3aa7a973646
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-26 22:45:17 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -3,7 +3,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := avc_bypass
 LOCAL_SRC_FILES := avc_bypass.c
-
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
 LOCAL_LDFLAGS := -fPIE -pie
 
