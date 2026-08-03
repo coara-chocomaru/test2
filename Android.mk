f2106ae73e39d0feb1d55c70c9886c0cad43e0ce
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-03 19:05:55 +0900

    Remove empty line in Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := avc_bypass
 LOCAL_SRC_FILES := avc_bypass.c
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
