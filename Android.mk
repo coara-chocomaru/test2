ad97e3869bd76b2a3c1f293cc66e9d5036e47eb8
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-05 06:29:06 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,6 @@
 # Android.mk
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := check_poc
 LOCAL_SRC_FILES := check_poc.c
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
