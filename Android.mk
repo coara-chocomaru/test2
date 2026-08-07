1978db77fe0da5840b003d4198b91dbc8969abbd
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-07 16:05:55 +0900

    Add asm.c to root_shell module sources

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := root_shell
-LOCAL_SRC_FILES := root_shell.c
+LOCAL_SRC_FILES := root_shell.c asm.c
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
 LOCAL_LDFLAGS := -fPIE -pie
 
