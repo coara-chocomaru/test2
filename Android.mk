aee7461a10a2271f40fab59d02fe44bf3c7b71ef
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-15 13:28:09 +0900

    Modify CFLAGS and LDFLAGS in Android.mk
    Updated compiler and linker flags for root_shell module.

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := root_shell
 LOCAL_SRC_FILES := root_shell.c
-LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
-LOCAL_LDFLAGS := -fPIE -pie
-
+LOCAL_CFLAGS := -std=gnu99 -O2 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -fstack-protector-strong
+LOCAL_LDFLAGS := -static
 include $(BUILD_EXECUTABLE)
