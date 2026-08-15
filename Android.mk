6ca47c8aab9f2aed8e56f2d06bd55da8d14b2c99
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-15 11:15:08 +0900

    Remove empty line before include directive

--- Android.mk
+++ Android.mk
@@ -5,5 +5,4 @@ LOCAL_MODULE := check_poc
 LOCAL_SRC_FILES := check_poc.c
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
 LOCAL_LDFLAGS := -fPIE -pie
-
 include $(BUILD_EXECUTABLE)
