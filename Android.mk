b7e73ce4f5695e1eefd4a270dc87e011731470ef
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-16 10:06:47 +0900

    Add pthread to linker flags in Android.mk

--- Android.mk
+++ Android.mk
@@ -1,10 +1,7 @@
-# Android.mk
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := root_shell
 LOCAL_SRC_FILES := root_shell.c
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
-LOCAL_LDFLAGS := -fPIE -pie
-
+LOCAL_LDFLAGS := -fPIE -pie -lpthread
 include $(BUILD_EXECUTABLE)
