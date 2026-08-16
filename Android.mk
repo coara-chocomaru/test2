fdc64cfe11d7adbf1e3dae87a0424230ca3b2bad
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-16 10:05:35 +0900

    Remove empty line in Android.mk

--- Android.mk
+++ Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := root_shell
 LOCAL_SRC_FILES := root_shell.c
-
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
 LOCAL_LDFLAGS := -fPIE -pie
 
