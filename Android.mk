fbd3413a32504f2e0bc5dcef0328639eb6636498
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-16 10:43:48 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := root_shell
 LOCAL_SRC_FILES := root_shell.c
-LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format -Wno-unused-parameter
+LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-parameter -Wno-unused-result
 LOCAL_LDFLAGS := -fPIE -pie
 
 include $(BUILD_EXECUTABLE)
