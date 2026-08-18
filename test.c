9080e76746b9ccbca0d1a5edaa9203efd848d092
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-19 02:48:52 +0900

    Replace Linux binder includes with seccomp.h

--- test.c
+++ test.c
@@ -27,8 +27,7 @@
 #include <sys/reboot.h>
 #include <stdint.h>
 #include <sys/fsuid.h>
-#include <linux/binder.h>
-#include <linux/android/binder.h>
+#include "seccomp.h"
 #include "binder.h"
 
 extern int setfsuid(uid_t);
