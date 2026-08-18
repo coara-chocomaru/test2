a78c3ab507d0b8a011a817bd0926a586b3b2e11b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-19 00:32:13 +0900

    Rename module and source file in Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := avc_bypass
-LOCAL_SRC_FILES := avc_bypass.c
+LOCAL_MODULE := test
+LOCAL_SRC_FILES := test.c
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
 LOCAL_LDFLAGS := -fPIE -pie
 
