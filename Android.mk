913c8519bf8619e03f4f9bd1ae9921e85bda4e28
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-20 06:10:12 +0900

    Add -ldl to LOCAL_LDFLAGS in Android.mk

--- Android.mk
+++ Android.mk
@@ -3,6 +3,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := test
 LOCAL_SRC_FILES := test.c
 LOCAL_CFLAGS := -fPIE -Wall -Wextra -O2 -Wno-unused-result -Wno-format
-LOCAL_LDFLAGS := -fPIE -pie
+LOCAL_LDFLAGS := -fPIE -pie -ldl
 
 include $(BUILD_EXECUTABLE)
