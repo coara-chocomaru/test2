ccff1a1823b498fe1db4306185dd0fff6633b6ad
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-20 16:50:17 +0900

    Optimize build flags for binder_test

--- Android.mk
+++ Android.mk
@@ -1,10 +1,7 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := binder_test
 LOCAL_SRC_FILES := binder_test.c
-LOCAL_CFLAGS := -Wall -Wextra -O0 -g
-LOCAL_LDFLAGS := -pthread
-
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_CFLAGS := -Wall -Wextra -O2 -static
 include $(BUILD_EXECUTABLE)
