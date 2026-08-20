456e0acf648a0adde78381c8cb405b00a91564b4
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-20 09:57:52 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -4,5 +4,4 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := rtsp_fuzzer
 LOCAL_SRC_FILES := rtsp_fuzzer.c
 LOCAL_CFLAGS := -Wall -O0 -g -fno-stack-protector -U_FORTIFY_SOURCE
-LOCAL_SHARED_LIBRARIES := libc
 include $(BUILD_EXECUTABLE)
