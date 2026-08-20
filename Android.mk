2028f64ca442d254ecdc4dfbc6a59261a5076d02
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-20 10:58:10 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := rtsp_fuzzer
-LOCAL_SRC_FILES := rtsp_fuzzer.c
-LOCAL_CFLAGS := -Wall -O0 -g -fno-stack-protector -U_FORTIFY_SOURCE
+LOCAL_MODULE := exploit_tester
+LOCAL_SRC_FILES := main.c tests.c utils.c
+LOCAL_CFLAGS := -Wall -Wextra -O0 -g
+LOCAL_LDFLAGS := -static
 include $(BUILD_EXECUTABLE)
