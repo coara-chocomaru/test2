0115ac5c44bdad6aaa263045408cce373da9dca2
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-20 09:15:50 +0900

    Add run_as_exploit_tester module to Android.mk

--- Android.mk
+++ Android.mk
@@ -1,18 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE            := exploit
-LOCAL_SRC_FILES         := exploit.c
-LOCAL_CFLAGS            := -fPIC -Wall -O2
-LOCAL_LDFLAGS           := -fPIC
-LOCAL_LDLIBS            := -ldl -llog
-include $(BUILD_SHARED_LIBRARY)
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := launcher
-LOCAL_SRC_FILES         := launcher.c
-LOCAL_CFLAGS            := -fPIE -Wall -O2
-LOCAL_LDFLAGS           := -fPIE -pie
+LOCAL_MODULE := run_as_exploit_tester
+LOCAL_SRC_FILES := run_as_exploit_tester.c
+LOCAL_CFLAGS := -O2 -Wall -pthread
+LOCAL_LDFLAGS := -pthread
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
